da5bdc601faddd1d1d50bb9d75e17a89ecf68dc9
Alfredo Interián <=>
2024-12-10 02:24:04 -0500

    Initializing Repository

---
+++ src/main/java/com/alfredointerian/personal_financies/Personal_Financies.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.alfredointerian.personal_financies;
+
+/**
+ *
+ * @author alfre
+ */
+public class Personal_Financies {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
