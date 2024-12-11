2f480084b20e54a06d86cbe5b83cd63233fe0b85
Alfredo Interián <=>
2024-12-11 17:56:22 -0500

    Open Client Authetication Form

--- src/main/java/com/alfredointerian/personal_financies/Personal_Financies.java
+++ src/main/java/com/alfredointerian/personal_financies/Personal_Financies.java
@@ -1,47 +1,36 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.alfredointerian.personal_financies;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.Scanner;
+import static com.alfredointerian.personal_financies.database.DatabaseManager.connect;
+import java.sql.ResultSet;
 
-import com.alfredointerian.personal_financies.auth.Session;
+import com.alfredointerian.personal_financies.client.Auth;
 
 /**
  *
  * @author alfre
  */
-
-
 public class Personal_Financies {
-    
 
     public static void main(String[] args) {
-        
-        Scanner read = new Scanner(System.in);
-        
-        System.out.println("""
-                           ||| Personal Financies - v0.0.0 |||
-                           by Alfredo Interián
-                           """);
-        
-        System.out.println("- Log In");
-        System.out.print("Email: ");
-        String email = read.next();
-        System.out.print("Password: ");
-        String password = read.next();
-        
-        Session user = new Session(email, password);
-        
-        if (user.getAccount_id() == -1) {
-            System.out.println("User couldn't be authenticated");
-        } else {
-            System.out.println("Welcome " + user.getAccount_id());
-        }  
+        Auth auth = new Auth();
+        auth.setVisible(true);
+    }
+    
+    private static void viewBankingAccounts () {
+        try (Connection conn = connect(); PreparedStatement bankingAccounts = conn.prepareStatement("SELECT * FROM banking_accounts")) {
+            ResultSet rs = bankingAccounts.executeQuery();
+            
+            while (rs.next()) {
+                System.out.println( rs.getString("bk_acc_name"));
+            }
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
     }
 }
