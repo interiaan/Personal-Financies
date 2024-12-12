b2741b3b9d7a12d10de25aabb1cdac457b5f50e7
Alfredo Interián <=>
2024-12-12 01:59:41 -0500

    Banking Account List Method removed This method is now in "BankingAccount"

--- src/main/java/com/alfredointerian/personal_financies/database/DatabaseManager.java
+++ src/main/java/com/alfredointerian/personal_financies/database/DatabaseManager.java
@@ -6,10 +6,7 @@ package com.alfredointerian.personal_financies.database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
 /**
  *
@@ -24,29 +21,5 @@ public class DatabaseManager {
      */
     public static Connection connect () throws SQLException {
        return DriverManager.getConnection("jdbc:sqlite:personal_financies.db");
-    }
-    
-    /**
-     * Get all banking accounts of the user account
-     * @param accountId Account ID related with the banking accounts
-     * @return ArrayList List with banking accounts
-     */
-    public static ArrayList<BankingAccount> getBankingAccounts (int accountId) {
-        ArrayList<BankingAccount> bankingAccounts = new ArrayList<>();
-        
-        try (Connection conn = connect();
-             PreparedStatement verify = conn.prepareStatement("SELECT * FROM banking_accounts WHERE bk_acc_owner = ?")) {
-            verify.setInt(1, accountId);
-            ResultSet account = verify.executeQuery();
-
-            while (account.next()) {
-                bankingAccounts.add(new BankingAccount(account.getString("bk_acc_name"), account.getInt("bk_acc_owner"))); // Adds banking account to list
-                bankingAccounts.getLast().setAccountId(account.getInt("bk_acc_id")); // Sets the last addtion adding the account id
-            }
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
-        }
-        
-        return bankingAccounts;
     }
 }
