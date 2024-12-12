1f042aacfc1c269cb5e0c93cc53d7e1fa6f75f2c
Alfredo Interián <=>
2024-12-12 01:59:05 -0500

    Banking Account List Method

--- src/main/java/com/alfredointerian/personal_financies/database/BankingAccount.java
+++ src/main/java/com/alfredointerian/personal_financies/database/BankingAccount.java
@@ -9,6 +9,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
  *
@@ -22,6 +23,10 @@ public class BankingAccount {
     public BankingAccount(String bankingAccountName, int bankingAccountOwner) {
        this.bankingAccountName = bankingAccountName;
        this.bankingAccountOwner = bankingAccountOwner;
+    }
+    
+    public int getAccountId () {
+        return this.bankingAccountId;
     }
     
     public void setAccountId (int accountId) {
@@ -56,5 +61,34 @@ public class BankingAccount {
         }
         
         return "[Banking account Name not found]";
+    }
+    
+    /**
+     * Get all banking accounts of the user account
+     * @param accountId Account ID related with the banking accounts
+     * @return ArrayList List with banking accounts
+     */
+    public static ArrayList<BankingAccount> fetchBankingAccounts (int accountId) {
+        ArrayList<BankingAccount> bankingAccounts = new ArrayList<>();
+        
+        try (Connection conn = connect();
+             PreparedStatement verify = conn.prepareStatement("SELECT * FROM banking_accounts WHERE bk_acc_owner = ?")) {
+            verify.setInt(1, accountId);
+            ResultSet account = verify.executeQuery();
+
+            while (account.next()) {
+                bankingAccounts.add(new BankingAccount(account.getString("bk_acc_name"), account.getInt("bk_acc_owner"))); // Adds banking account to list
+                bankingAccounts.getLast()
+                        .setAccountId(account.getInt("bk_acc_id")); // Sets the last addtion adding the account id
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        
+        return bankingAccounts;
+    }
+
+    public String getBankingAccountName() {
+        return this.bankingAccountName;
     }
 }
